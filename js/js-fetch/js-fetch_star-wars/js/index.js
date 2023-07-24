07020cf61295266599bd2154b26d9d04dc1a9da6
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-07-24 10:36:02 +0200

    monday-24-morning

--- js/js-fetch/js-fetch_star-wars/js/index.js
+++ js/js-fetch/js-fetch_star-wars/js/index.js
@@ -37,10 +37,16 @@ const EXAMPLE_DATA = {
 const firstCard = Card(EXAMPLE_DATA);
 renderElement(firstCard);
 
-fetchDataAndRender();
-
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchPeopleData() {
+  try {
+    const response = await fetch("https://swapi.dev/api/people");
+    const data = await response.json();
+    console.log(data.results);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
+
+fetchPeopleData();

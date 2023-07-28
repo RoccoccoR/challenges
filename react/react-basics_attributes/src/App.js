4be5b1bab202481668c91b67433961d1843480a1
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-07-28 11:51:09 +0200

    yo

--- react/react-basics_attributes/src/App.js
+++ react/react-basics_attributes/src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  return <Createarticle></Createarticle>;
+}
+
+function Createarticle() {
   return (
     <div>
       <article className="article">
-        <h2 className="article__title">React is amaZing!</h2>
+        <h2 className="article__title">React is amaZing!!</h2>
         <label htmlFor="inputField">Subscribe here:</label>
         <input type="text" id="inputField" />
         <a

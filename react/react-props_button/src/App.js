8d2dd3cdf840f2c23ca61261cecd569578f80c54
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-07-28 17:33:49 +0200

    lastdone-beer time now!

--- react/react-props_button/src/App.js
+++ react/react-props_button/src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  const handleClick = () => {
+    alert("You clicked me!");
+  };
+
+  return (
+    <Button
+      text="Click me!"
+      color="#fff200"
+      disabled={false}
+      onClick={handleClick}
+    />
+  );
+}
+
+function Button({ text, color, disabled, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      style={{ height: "300px", width: "300px", backgroundColor: color }}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
 }

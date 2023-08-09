6c465c6828a43a07b9929021f05cf034854e4a39
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-09 10:43:33 +0200

    ntx.ja challange new name

---
+++ lotr-app-fix/src/pages/volumes/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { introduction, volumes } from "../../../lib/data.js";
+import Link from "next/link";
+
+export default function Volumes() {
+  return (
+    <div>
+      <h1>Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <h2>All Volumes</h2>
+      <ul>
+        {volumes.map((volume) => {
+          return (
+            <li key={volume.title}>
+              <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}

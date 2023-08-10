5f1f009f16c3b1c3daf588472f33ce23118aa436
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-10 12:26:25 +0200

    crazy

--- react-styled-components_box/pages/index.js
+++ react-styled-components_box/pages/index.js
@@ -1,10 +1,15 @@
-import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import React from "react";
+import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName";
+import BoxWithStyledComponents from "../components/BoxWithStyledComponents";
 
-export default function HomePage() {
+export default function Home() {
   return (
     <div>
+      <h1>Styled Components are Crazy</h1>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
+      <BoxWithStyledComponents />
+      <BoxWithStyledComponents isBlack />
     </div>
   );
 }

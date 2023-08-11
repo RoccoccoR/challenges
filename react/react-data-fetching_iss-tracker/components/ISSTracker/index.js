96b52319940fb8363132c5ed183009a664f0cc58
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-11 15:24:30 +0200

    isstrakerok

--- react/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,45 +1,64 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
-const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
+  const { data, error, isLoading } = useSWR(
+    "https://api.wheretheiss.at/v1/satellites/25544",
+    fetcher
+  );
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
+  // render data
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
-      <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
-      />
+      <Map longitude={data.longitude} latitude={data.latitude} />
+      <Controls longitude={data.longitude} latitude={data.latitude} />
     </main>
   );
 }
+
+// export default function ISSTracker() {
+// const [coords, setCoords] = useState({
+//   longitude: 0,
+//   latitude: 0,
+// });
+
+// async function getISSCoords() {
+//   try {
+//     const response = await fetch(URL);
+//     if (response.ok) {
+//       const data = await response.json();
+//       setCoords({ longitude: data.longitude, latitude: data.latitude });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// useEffect(() => {
+//   const timer = setInterval(() => {
+//     getISSCoords();
+//   }, 5000);
+
+//   return () => {
+//     clearInterval(timer);
+//   };
+// }, []);
+
+// return (
+//   <main>
+//     <Map longitude={coords.longitude} latitude={coords.latitude} />
+//     <Controls
+//       longitude={coords.longitude}
+//       latitude={coords.latitude}
+//       onRefresh={getISSCoords}
+//     />
+//   </main>
+// );
+// }

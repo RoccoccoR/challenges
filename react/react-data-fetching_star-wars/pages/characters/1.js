4e357397d7c982b1763012c61fd7dc72111a4dc2
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-13 19:17:12 +0200

    closing

--- react/react-data-fetching_star-wars/pages/characters/1.js
+++ react/react-data-fetching_star-wars/pages/characters/1.js
@@ -1,9 +1,18 @@
 import Card from "../../components/Card";
+import useSWR from "swr";
 import Layout from "../../components/Layout";
 
 export default function Character() {
+  const { data, error, isLoading } = useSWR(
+    "https://swapi.dev/api/people/${id}"
+  );
+
   const id = 1;
 
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  // render data
   return (
     <Layout>
       <Card

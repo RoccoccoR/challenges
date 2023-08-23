01ee5c76f0cd7d4145a1f07c19fed0727447cae1
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-23 15:13:32 +0200

    a

--- backend/backend-read_products/pages/api/products/[id].js
+++ backend/backend-read_products/pages/api/products/[id].js
@@ -1,13 +1,17 @@
-import { products } from "../../../lib/products";
+import Product from "../../../db/models/Product";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
+  try {
+    const product = await Product.findById(id);
 
-  if (!product) {
-    return response.status(404).json({ status: "Not Found" });
+    if (!product) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    response.status(200).json(product);
+  } catch (error) {
+    response.status(500).json({ status: "Internal Server Error" });
   }
-
-  response.status(200).json(product);
 }
